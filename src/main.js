1a3945fa4afd45f84128f33593594f40d53889fd
MaestroYaco <maestro-yaco@hotmail.com>
2025-01-24 20:22:44 -0500

    docs: V1.0.0.1

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import './backjack';
 import { startGame } from './backjack/usecases/start-game.js';
 
 
-// //Inicia el juego ya preconfigurado.
+// //Inicia el juego ya preconfigurado. 2025 by Yaco
 startGame();
